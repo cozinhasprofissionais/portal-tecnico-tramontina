44d886d4be4078a3369d4a8bac7ea58e318f30c2
cozinhasprofissionais <rafael.silva4@tramontina.com>
2025-12-30 16:15:29 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,26 @@
+const dados = {
+  contatos: {
+    titulo: "Contatos Tramontina",
+    link: "https://cozinhasprofissionais.github.io/canais-atendimento-tramontina/"
+  },
+
+  boletins: [
+    { titulo: "Cozinhas Profissionais (PDF)", link: "pdf/boletim-cozinhas-profissionais.pdf" },
+    { titulo: "Fornos Rational (PDF)", link: "pdf/boletim-fornos-rational.pdf" }
+  ],
+
+  catalogo: {
+    titulo: "Catálogo de Produtos",
+    link: "pdf/catalogo-produtos.pdf"
+  },
+
+  limpeza: [
+    { titulo: "Linha Europa", link: "https://www.youtube.com/watch?v=GdR_g1C6QRQ" },
+    { titulo: "Linha América", link: "https://www.youtube.com/watch?v=9j0oj0zutR0" }
+  ],
+
+  manutencao: [
+    { titulo: "Linha Europa", link: "https://www.youtube.com/watch?v=vzmtNESyezE" },
+    { titulo: "Linha América", link: "https://www.youtube.com/watch?v=2z3SMHY_ZCs" }
+  ]
+};
