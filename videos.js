399b42c54788f9dcb3ca2d9de2eca634cbf64e98
cozinhasprofissionais <rafael.silva4@tramontina.com>
2025-12-30 16:29:25 -0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,23 +1,188 @@
 const videos = {
-  limpeza: [
-    {
-      titulo: "Linha Europa – Utilização e Limpeza",
-      link: "https://www.youtube.com/watch?v=GdR_g1C6QRQ"
-    },
-    {
-      titulo: "Linha América – Utilização e Limpeza",
-      link: "https://www.youtube.com/watch?v=9j0oj0zutR0"
-    }
-  ],
 
-  manutencao: [
-    {
-      titulo: "Linha Europa – Manutenção",
-      link: "https://www.youtube.com/watch?v=vzmtNESyezE"
-    },
-    {
-      titulo: "Linha América – Manutenção",
-      link: "https://www.youtube.com/watch?v=2z3SMHY_ZCs"
-    }
-  ]
+  /* =========================
+     LIMPEZA / UTILIZAÇÃO
+     ========================= */
+
+  limpeza: {
+
+    europa: [
+      {
+        titulo: "Utilização e Limpeza Banho Maria | Europa",
+        link: "https://www.youtube.com/watch?v=GdR_g1C6QRQ"
+      },
+      {
+        titulo: "Utilização e Limpeza Chapa Cromada | Europa",
+        link: "https://youtu.be/DiqutDQ6BDI"
+      },
+      {
+        titulo: "Utilização e Limpeza Chapa de Ferro | Europa",
+        link: "https://youtu.be/vbZnJNG4CsU"
+      },
+      {
+        titulo: "Utilização e Limpeza Char Broiler | Europa",
+        link: "https://youtu.be/YkVxM2YjUmA"
+      },
+      {
+        titulo: "Utilização e Limpeza Cozedor de Massa | Europa",
+        link: "https://youtu.be/oQKWLkkiKfs"
+      },
+      {
+        titulo: "Utilização e Limpeza Fogão a Gás | Europa",
+        link: "https://youtu.be/YvuD1BH76D4"
+      },
+      {
+        titulo: "Utilização e Limpeza Fogão a Indução | Europa",
+        link: "https://youtu.be/BgqPur2XIt4"
+      },
+      {
+        titulo: "Utilização e Limpeza Fogão Elétrico | Europa",
+        link: "https://youtu.be/gEkoX9hhOig"
+      },
+      {
+        titulo: "Utilização e Limpeza Fogão Francês | Europa",
+        link: "https://youtu.be/gjFRYxBhaqo"
+      },
+      {
+        titulo: "Utilização e Limpeza Fritadeira a Gás | Europa",
+        link: "https://youtu.be/yJn9f-y-vxU"
+      },
+      {
+        titulo: "Utilização e Limpeza Fritadeira Elétrica | Europa",
+        link: "https://youtu.be/7AFQj1zPTzI"
+      },
+      {
+        titulo: "Utilização e Limpeza Refrigeradores | Europa",
+        link: "https://youtu.be/5AjJnTv-uMs"
+      }
+    ],
+
+    america: [
+      {
+        titulo: "Apresentação | América",
+        link: "https://www.youtube.com/watch?v=9j0oj0zutR0"
+      },
+      {
+        titulo: "Apresentação Broiler | América",
+        link: "https://www.youtube.com/watch?v=KvNtNA75h8w"
+      },
+      {
+        titulo: "Utilização Broiler | América",
+        link: "https://www.youtube.com/watch?v=aBUoRItAqxI"
+      },
+      {
+        titulo: "Limpeza Broiler | América",
+        link: "https://www.youtube.com/watch?v=3NmYR3Kd6iI"
+      },
+      {
+        titulo: "Apresentação Fritadeira Elétrica | América",
+        link: "https://www.youtube.com/watch?v=Quyqey8ZW6o"
+      },
+      {
+        titulo: "Utilização Fritadeira Elétrica | América",
+        link: "https://www.youtube.com/watch?v=ym_z3YZ66Yg"
+      },
+      {
+        titulo: "Limpeza Fritadeira Elétrica | América",
+        link: "https://www.youtube.com/watch?v=GepBp3Iu2D0"
+      },
+      {
+        titulo: "Apresentação Chapa a Gás | América",
+        link: "https://www.youtube.com/watch?v=WgVrbCWT-pY"
+      },
+      {
+        titulo: "Utilização Chapa a Gás | América",
+        link: "https://www.youtube.com/watch?v=wdB9M8gXBWk"
+      },
+      {
+        titulo: "Limpeza Chapa a Gás | América",
+        link: "https://www.youtube.com/watch?v=Zcdho2A0N_I"
+      },
+      {
+        titulo: "Apresentação Fogão a Gás | América",
+        link: "https://www.youtube.com/watch?v=vaElDWldBfc"
+      },
+      {
+        titulo: "Utilização Fogão a Gás | América",
+        link: "https://www.youtube.com/watch?v=QWUN5xbPXf8"
+      },
+      {
+        titulo: "Limpeza Fogão a Gás | América",
+        link: "https://www.youtube.com/watch?v=MfeRtPk2m1g"
+      },
+      {
+        titulo: "Apresentação Cozedor de Massa | América",
+        link: "https://www.youtube.com/watch?v=iEA4pUhDXEg"
+      },
+      {
+        titulo: "Utilização Cozedor de Massa | América",
+        link: "https://www.youtube.com/watch?v=EoxWIx0uEVs"
+      },
+      {
+        titulo: "Limpeza Cozedor de Massa | América",
+        link: "https://www.youtube.com/watch?v=o5wo-1CFwho"
+      },
+      {
+        titulo: "Apresentação Fogão a Indução | América",
+        link: "https://www.youtube.com/watch?v=mnXMPK53obc"
+      },
+      {
+        titulo: "Utilização Fogão a Indução | América",
+        link: "https://www.youtube.com/watch?v=Md3lLKpyMDQ"
+      },
+      {
+        titulo: "Limpeza Fogão a Indução | América",
+        link: "http://youtube.com/watch?v=_KiJnge99W8"
+      },
+      {
+        titulo: "Apresentação Fritadeira a Gás | América",
+        link: "https://www.youtube.com/watch?v=N4UY1FncUhM"
+      },
+      {
+        titulo: "Utilização Fritadeira a Gás | América",
+        link: "https://www.youtube.com/watch?v=6gRJLPBlMN4"
+      },
+      {
+        titulo: "Limpeza Fritadeira a Gás | América",
+        link: "https://www.youtube.com/watch?v=TqigR7vLe2Q"
+      }
+    ]
+  },
+
+  /* =========================
+     MANUTENÇÃO (AUTORIZADOS)
+     ========================= */
+
+  manutencao: {
+
+    europa: [
+      { titulo: "Técnico Fogão a Gás | Europa", link: "https://www.youtube.com/watch?v=vzmtNESyezE" },
+      { titulo: "Técnico Fritadeira Elétrica | Europa", link: "https://www.youtube.com/watch?v=3OAsU5o4aQA" },
+      { titulo: "Técnico Refrigerador e Congelador Star/Harmony | Europa", link: "https://www.youtube.com/watch?v=cpHdH-_A7W8" },
+      { titulo: "Técnico Char Broiler a Gás | Europa", link: "https://www.youtube.com/watch?v=VcI7Nivhql8" },
+      { titulo: "Técnico Chapa a Gás | Europa", link: "https://www.youtube.com/watch?v=4hco2yG7JA0" },
+      { titulo: "Técnico Cozedor de Massa a Gás | Europa", link: "https://www.youtube.com/watch?v=jJ8Rrx2ui6k" },
+      { titulo: "Técnico Supercongelador | Europa", link: "https://www.youtube.com/watch?v=vxxfbqJPFVk" },
+      { titulo: "Técnico Balcão Refrigerado | Europa", link: "https://www.youtube.com/watch?v=FccuEKRFHTk" },
+      { titulo: "Cozedor de Massa a Gás | Europa", link: "https://youtu.be/gWtqqoqr-bE" },
+      { titulo: "Fogão a Gás | Europa", link: "https://youtu.be/2G21vUuazt0" },
+      { titulo: "Chapa a Gás | Europa", link: "https://youtu.be/UOoBysQflfA" },
+      { titulo: "Char Broiler a Gás | Europa", link: "https://youtu.be/MyNXtPgV2Rg" },
+      { titulo: "Supercongelador | Europa", link: "https://youtu.be/5yop--eTfQc" },
+      { titulo: "Balcão Refrigerado | Europa", link: "https://youtu.be/gKG1wRSkPaQ" },
+      { titulo: "Refrigerador e Congelador Star e Harmony | Europa", link: "https://youtu.be/iF2-9PkXYa8" },
+      { titulo: "Fritadeira Elétrica | Europa", link: "https://youtu.be/eRc6KcxkZV0" }
+    ],
+
+    america: [
+      { titulo: "Instalação e Manutenção Fogão a Gás | América", link: "https://www.youtube.com/watch?v=2z3SMHY_ZCs" },
+      { titulo: "Instalação e Manutenção Forno a Gás | América", link: "https://www.youtube.com/watch?v=4TBwOydk5Hc" },
+      { titulo: "Instalação e Manutenção Chapa a Gás | América", link: "https://www.youtube.com/watch?v=1-eCshR_SVw" },
+      { titulo: "Instalação e Manutenção Char Boiler | América", link: "https://www.youtube.com/watch?v=k47ZCWBt8bU" },
+      { titulo: "Instalação e Manutenção Fritadeira a Gás | América", link: "https://www.youtube.com/watch?v=d0h9gHbyNqA" },
+      { titulo: "Instalação e Manutenção Cozedor de Massa | América", link: "https://www.youtube.com/watch?v=qKVRvOPqPvE" },
+      { titulo: "Instalação e Manutenção Fritadeira Elétrica | América", link: "https://www.youtube.com/watch?v=PwuxIv9eJG4" },
+      { titulo: "Instalação e Manutenção Banho Maria | América", link: "https://www.youtube.com/watch?v=f8IC-jOmDag" }
+    ]
+  }
 };
