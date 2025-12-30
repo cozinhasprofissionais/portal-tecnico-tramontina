57793303b31b2f5ecf63df2e6d93fc4c3da7bd52
cozinhasprofissionais <rafael.silva4@tramontina.com>
2025-12-30 16:26:41 -0300

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,23 @@
+const videos = {
+  limpeza: [
+    {
+      titulo: "Linha Europa – Utilização e Limpeza",
+      link: "https://www.youtube.com/watch?v=GdR_g1C6QRQ"
+    },
+    {
+      titulo: "Linha América – Utilização e Limpeza",
+      link: "https://www.youtube.com/watch?v=9j0oj0zutR0"
+    }
+  ],
+
+  manutencao: [
+    {
+      titulo: "Linha Europa – Manutenção",
+      link: "https://www.youtube.com/watch?v=vzmtNESyezE"
+    },
+    {
+      titulo: "Linha América – Manutenção",
+      link: "https://www.youtube.com/watch?v=2z3SMHY_ZCs"
+    }
+  ]
+};
